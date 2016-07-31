452c83a0afee7dbfc82f2d88eb685e379fcf21b0
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-07-30 19:39:23 -0500

    auto upload file from selection

--- school/forms.py
+++ school/forms.py
@@ -1,5 +1,7 @@
+from django.forms import ModelForm, Form
 from django import forms
+from audio.models import *
 
-class AudioForm(forms.Form):
-    file = forms.FileField()
+class AudioForm(Form):
     name = forms.CharField(max_length=30)
+    file = forms.FileField()
