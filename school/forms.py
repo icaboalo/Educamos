11081f73938c74c2957ae94dcd1ef6ad9c4ed341
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-07-31 09:45:56 -0500

    modified styles audio upload

--- school/forms.py
+++ school/forms.py
@@ -3,5 +3,6 @@ from django import forms
 from audio.models import *
 
 class AudioForm(Form):
-    name = forms.CharField(max_length=30, label='', widget=forms.TextInput(attrs={'placeholder': 'Nombre'}))
-    file = forms.FileField(label='')
+    name = forms.CharField(max_length=30, label='', widget=forms.TextInput(attrs={'placeholder': 'Nombre',
+                                                                                  'class': 'textbox', 'required': ''}))
+    file = forms.FileField(label='', widget=forms.FileInput(attrs={'required': ''}))
