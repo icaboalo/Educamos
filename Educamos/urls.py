61dca84e6219e17fb1a35115271ddefd7f6b80cd
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-07-31 10:47:42 -0500

    modified base url

--- Educamos/urls.py
+++ Educamos/urls.py
@@ -23,7 +23,7 @@ from user import views as user_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', user_views.login_frontend),
+    url(r'^/', user_views.login_frontend),
     url(r'login/$', user_views.login_frontend, name='login'),
     url(r'register/$', user_views.register, name='register'),
 
